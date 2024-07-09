63d50569e93993e01848c923684853a1832bbcca
Sreetam <sreetam.dev@colourmoon.com>
2024-07-10 00:34:51 +0530

    about us page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import About_us
 
 # Create your views here.
 
 def about_us(request):
-    return render(request, 'home/about.html')
+    about_us = About_us.objects.get()
+    return render(request, 'home/about.html',{
+        "about_us": about_us
+    })
